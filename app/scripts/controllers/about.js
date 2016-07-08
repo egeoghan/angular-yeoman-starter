611f05851cbb948da9b8a1c7b1b49d06c2eb3f45
Erin Geoghan <eringeoghan@Erins-MacBook-Pro.local>
2016-07-08 16:22:07 -0400

    start project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ballsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ballsApp
+ */
+angular.module('ballsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ballsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ballsApp
+ */
+angular.module('ballsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
